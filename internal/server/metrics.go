8bde62578c251e802885ed2dc62dfdb7debf1c0e
Christophe Lambin <christophe.lambin@gmail.com>
2026-03-30 22:47:15 +0200

    add grafana dashboard

--- internal/server/metrics.go
+++ internal/server/metrics.go
@@ -27,9 +27,8 @@ func GetMetrics() Metrics {
 		),
 		duration: prometheus.NewSummaryVec(
 			prometheus.SummaryOpts{
-				Name: "http_request_duration_seconds",
-				Help: "request duration in seconds",
-				// TODO
+				Name:       "http_request_duration_seconds",
+				Help:       "request duration in seconds",
 				Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
 			},
 			[]string{"handler", "code", "method"},
