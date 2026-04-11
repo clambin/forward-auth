68abe0d8dd41b8faf80209612ba2d6734d19bc22
Christophe Lambin <christophe.lambin@gmail.com>
2026-04-11 17:29:57 +0200

    disable cloudflare caching block

--- internal/server/web/web.go
+++ internal/server/web/web.go
@@ -16,10 +16,12 @@ func New() http.Handler {
 	}
 	h := http.FileServer(http.FS(sub))
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO: this is to prevent cloudflare from caching the static files while we're in active development.
-		/// To be revisited once app development settles down.
-		w.Header().Set("Cache-Control", "no-store")
-		h.ServeHTTP(w, r)
-	})
+	// this prevents cloudflare from caching the static files while we're in active development.
+	if false {
+		h = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Cache-Control", "no-store")
+			h.ServeHTTP(w, r)
+		})
+	}
+	return h
 }
