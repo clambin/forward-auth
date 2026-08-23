88311c9fd91dc599b5359e41c660f527376efad9
Christophe Lambin <christophe.lambin@gmail.com>
2026-08-23 18:39:09 +0200

    refactor: simplify Metrics.InstrumentedHandler() in metrics.go by currying the metrics with the handler label directly.

--- internal/server/middleware/metrics.go
+++ internal/server/middleware/metrics.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"context"
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -52,44 +51,9 @@ func (m Metrics) Collect(ch chan<- prometheus.Metric) {
 
 // InstrumentedHandler returns a middleware that instruments the given handler with the counter and duration metrics.
 func (m Metrics) InstrumentedHandler(handler string) func(http.Handler) http.Handler {
+	counter := m.counter.MustCurryWith(prometheus.Labels{"handler": handler})
+	duration := m.duration.MustCurryWith(prometheus.Labels{"handler": handler})
 	return func(next http.Handler) http.Handler {
-		return withHandlerInCtx(handler)(
-			instrumentedHandlerCounter(m.counter)(
-				instrumentedHandlerDuration(m.duration)(next),
-			),
-		)
-	}
-}
-
-type handlerCtxKey struct{}
-
-func handlerFromCtx(ctx context.Context) string {
-	return ctx.Value(handlerCtxKey{}).(string)
-}
-
-// withHandlerInCtx returns a middleware that adds the given handler name to the request context,
-// so that it can be extracted by instrumentedHandlerCounter and instrumentedHandlerDuration.
-func withHandlerInCtx(handler string) func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := context.WithValue(r.Context(), handlerCtxKey{}, handler)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
-}
-
-// instrumentedHandlerCounter returns a promhttp.InstrumentHandlerCounter handler with an added "handler" label,
-// whose value is extracted from the request context.
-func instrumentedHandlerCounter(c *prometheus.CounterVec) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return promhttp.InstrumentHandlerCounter(c, next, promhttp.WithLabelFromCtx("handler", handlerFromCtx))
-	}
-}
-
-// instrumentedHandlerDuration returns a promhttp.InstrumentHandlerDuration handler with an added "handler" label,
-// whose value is extracted from the request context.
-func instrumentedHandlerDuration(c prometheus.ObserverVec) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return promhttp.InstrumentHandlerDuration(c, next, promhttp.WithLabelFromCtx("handler", handlerFromCtx))
+		return promhttp.InstrumentHandlerCounter(counter, promhttp.InstrumentHandlerDuration(duration, next))
 	}
 }
